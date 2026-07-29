5a3f69377567ec21fea380b84f3a100b5186accc
Rudra Verma <rudraverma781@gmail.com>
2026-07-29 18:48:34 +0530

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.js
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortArray(nums) {
+        nums.sort((a,b)=>a-b)
+        return nums
+        
+    }
+}
