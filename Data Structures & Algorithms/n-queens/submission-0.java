63a2bb96d1b8d5c463af2a27fd462f7208231608
Rudra Verma <rudraverma781@gmail.com>
2026-08-15 08:19:28 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.java
@@ -0,0 +1,78 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+                List<List<String>> ans = new ArrayList<>();
+
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        solve(0, board, ans, n);
+
+        return ans;
+    }
+        private void solve(int row, char[][] board,
+                       List<List<String>> ans, int n) {
+
+        if (row == n) {
+            List<String> temp = new ArrayList<>();
+
+            for (char[] r : board) {
+                temp.add(new String(r));
+            }
+
+            ans.add(temp);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+
+            if (isSafe(row, col, board, n)) {
+
+                board[row][col] = 'Q';
+
+                solve(row + 1, board, ans, n);
+
+                board[row][col] = '.';
+            }
+        }
+    }
+    private boolean isSafe(int row, int col,
+                           char[][] board, int n) {
+
+        // same column
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // upper-left diagonal
+        int i = row - 1;
+        int j = col - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+            i--;
+            j--;
+        }
+
+        // upper-right diagonal
+        i = row - 1;
+        j = col + 1;
+
+        while (i >= 0 && j < n) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+            i--;
+            j++;
+        }
+
+        return true;
+    }
+
+}
